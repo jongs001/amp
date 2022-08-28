a8ff00ef177fc6932c5cccd0c607b15bf13f33d2
jongs001 <jongs001@mymail.unisa.edu.au>
2022-08-28 13:15:47 +0930

    update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
         "ampf680c775": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
