1381b10a97004312fc81bb5c6030d9b0652e54b9
jongs001 <jongs001@mymail.unisa.edu.au>
2022-08-30 16:56:36 +0930

    auth update

--- src/App.js
+++ src/App.js
@@ -12,33 +12,17 @@ function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
+ 
   async function onChange(e) {
     if (!e.target.files[0]) return
     const file = e.target.files[0];
     setFormData({ ...formData, image: file.name });
     await Storage.put(file.name, file);
-    fetchNotes();
+
   }
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
-
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
+  async function uploadDocument() {
+    if (!formData.name || !formData.description || !formData.image) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
     if (formData.image) {
       const image = await Storage.get(formData.image);
@@ -48,47 +32,37 @@ function App() {
     setFormData(initialFormState);
   }
 
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
-
   return (
-    <div className="App">
-      <h1>My Notes App</h1>
+    
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h1 class="masthead-brand">N&#252;ber</h1>
+        <nav class="nav nav-masthead justify-content-center">
+        </nav>
+      </div>
+    </header>
+      <h1>Upload Driver Identity Documents</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Document name"
         value={formData.name}
       />
       <input
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
+        placeholder="Document description"
         value={formData.description}
       />
       <input
         type="file"
         onChange={onChange}
       />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-      {
-        notes.map(note => (
-          <div key={note.id || note.name}>
-            <h2>{note.name}</h2>
-            <p>{note.description}</p>
-            <button onClick={() => deleteNote(note)}>Delete note</button>
-            {
-              note.image && <img src={note.image} style={{width: 400}} />
-            }
-          </div>
-        ))
-      }
-      </div>
+      <button onClick={uploadDocument}>Upload Documents</button>
       <AmplifySignOut />
     </div>
   );
 }
 
 export default withAuthenticator(App);
+//export default App;
